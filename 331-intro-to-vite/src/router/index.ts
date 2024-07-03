c52ad107763702f36465d5e4214b7e549ebd74d6
Wora2001 <woraphon_ph@cmu.ac.th>
2024-07-03 15:46:04 +0700

    Edit

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/EventListView.vue'
 import EventListView from '../views/EventListView.vue'
 
 const router = createRouter({
