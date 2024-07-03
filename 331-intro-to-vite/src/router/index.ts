da714db6d1e7ea1a45cdeccdae2a5e88281a0b95
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-07-03 22:44:07 +0700

    Final 16.3

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
+import StudentListView from '@/views/StudentListView.vue'
+import path from 'path'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentListView
     }
   ]
 })
