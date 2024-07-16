ad51eac7cf0d8ec801bc9df687cdf257955f11c9
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-07-16 20:02:03 +0700

    Update StudentService.ts
    Edit for start lab03

--- 331-intro-to-vite/src/services/StudentService.ts
+++ 331-intro-to-vite/src/services/StudentService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://dv-student-backend-2019.appspot.com/',
+  baseURL: 'https://dv-student-backend-2019.appspot.com',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
