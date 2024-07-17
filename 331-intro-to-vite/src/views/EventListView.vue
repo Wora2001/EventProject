8c1cb27cce56e8c943530352bf062b70632cbe86
Anuphat5056 <anuphat_m@cmu.ac.th>
2024-07-17 15:45:16 +0700

    2.7

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,14 +2,14 @@
 import EventCard from '@/components/EventCard.vue'
 import EventInfo from '@/components/EventInfo.vue'
 import Event from '@/types/Event'
-import { ref, onMounted, watchEffect } from 'vue';
+import { ref, onMounted, watchEffect, computed } from 'vue';
 import EventService from '@/services/EventService'
 import type { Axios, AxiosResponse } from 'axios';
 import type { EventItem } from '@/type';
 import { RouterLink } from 'vue-router';
 
 const events = ref<Event[]>(null)
-
+const totalEvent = ref<number>(0)
 
     const props = defineProps({
       page: {
@@ -18,15 +18,17 @@ const events = ref<Event[]>(null)
       }
     })
 
-    // EventService.getEvents(2, props.page)
-    // .then((response: AxiosResponse<EventItem[]>) => {
-    //   events.value = response.data
-    // })
+    const hasNextPage = computed(() => {
+      //first calculate the total
+      const totalPages = Math.ceil(totalEvent.value / 2)
+      return props.page.valueOf() < totalPages
+    })
 
     watchEffect (() => {
         EventService.getEvents(2, props.page)
       .then((response: AxiosResponse<EventItem[]>) => {
         events.value = response.data
+        totalEvent.value = response.headers['x-total-count']
       })
     })
     
@@ -39,7 +41,7 @@ const events = ref<Event[]>(null)
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
     <RouterLink :to="{name: 'event-list-view', query: { page: page - 1} }" rel="prev" v-if="page != 1">Prev page</RouterLink>
-    <RouterLink :to="{name: 'event-list-view', query: { page: page + 1} }" rel="next" v-if="page + 1">Next page</RouterLink>
+    <RouterLink :to="{name: 'event-list-view', query: { page: page + 1} }" rel="next" v-if="hasNextPage">Next page</RouterLink>
     
   </div>
 </template>
